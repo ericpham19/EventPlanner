941e31e2d974fa6a670a05856dfb06feb96901a2
Duy Pham <ericpham19@outlook.com>
2022-11-09 16:46:14 +1100

    added a few controller tests

---
+++ spec/controllers/booking_types_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+  RSpec.describe BookingTypesController, type: :controller do
+    describe "GET index" do
+        it "returns a successful response" do
+            get :index
+            expect(response).to be_successful 
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+end
+        
